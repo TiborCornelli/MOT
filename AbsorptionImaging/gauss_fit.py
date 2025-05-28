766bfed473b76ba3488a4f275a164be5a9b21e0e
TiborCornelli <tibor.cornelli@t-online.de>
2025-05-28 10:59:48 +0200

    Chanced time stamps

--- AbsorptionImaging/gauss_fit.py
+++ AbsorptionImaging/gauss_fit.py
@@ -30,8 +30,13 @@ x0_values = []
 sigma_x_values = []
 y0_values = []
 sigma_y_values = []
+dt = 250 * 1e-6  # time step in seconds
+start_tof = 71 * 1e-6
+time_stamps = np.arange(len(file_list)) * dt
 
-for file in file_list[:1]:
+# ------- Fitting Gaussian to projections -------
+
+for file in file_list:
     img = read_gz_file(file, returnRaw=False, returnDict=False)
     cropped = img[slice(*roi["yMain"]), slice(*roi["xMain"])]
     projection_x = np.sum(cropped, axis=0)
@@ -40,44 +45,160 @@ for file in file_list[:1]:
     popt_x, _ = curve_fit(gaussian, x_vals_centered, projection_x)
     popt_y, _ = curve_fit(gaussian, y_vals_centered, projection_y)
 
-    file_indices.append(int(file[-5:-3]))
+    file_index = int(file[-5:-3])
+    file_indices.append(file_index)
     x0_values.append(popt_x[1] + x_length / 2 + roi["xMain"][0])
-    sigma_x_values.append(popt_x[2])
+    sigma_x_values.append(abs(popt_x[2]))
     y0_values.append(popt_y[1] + y_length / 2 + roi["yMain"][0])
-    sigma_y_values.append(popt_y[2])
+    sigma_y_values.append(abs(popt_y[2]))
 
-    # plt.plot(x_vals_centered, projection_x, label="X Projection")
-    # plt.plot(
-    #     x_vals_centered,
-    #     gaussian(x_vals_centered, *popt_x),
-    #     label="Gaussian Fit X",
-    #     linestyle="--",
-    # )
-    # plt.title(f"Gaussian Fit X for {file[-5:-3]}")
-    # plt.show()
 
-# Plot x0 and sigma_x values
-plt.subplot(2, 2, 1)
-plt.scatter(file_indices, x0_values, label="x0")
-plt.xlabel("File Index")
-plt.ylabel("x0 Value")
-plt.title("x0 Values")
-plt.subplot(2, 2, 2)
-plt.scatter(file_indices, sigma_x_values, label="sigma_x")
-plt.xlabel("File Index")
-plt.ylabel("sigma_x Value")
-plt.title("sigma_x Values")
-plt.tight_layout()
-# Plot y0 and sigma_y values in the right column of the same figure
-plt.subplot(2, 2, 3)
-plt.scatter(file_indices, y0_values, label="y0")
-plt.xlabel("File Index")
-plt.ylabel("y0 Value")
-plt.title("y0 Values")
-plt.subplot(2, 2, 4)
-plt.scatter(file_indices, sigma_y_values, label="sigma_y")
-plt.xlabel("File Index")
-plt.ylabel("sigma_y Value")
-plt.title("sigma_y Values")
-plt.tight_layout()
+# Example
+plt.plot(
+    x_vals_centered + x_length / 2 + roi["xMain"][0], projection_x, label="X Projection"
+)
+plt.plot(
+    x_vals_centered + x_length / 2 + roi["xMain"][0],
+    gaussian(x_vals_centered, *popt_x),
+    label="Gaussian Fit X",
+    linestyle="--",
+    linewidth=3,
+)
+plt.title(f"Gaussian Fit along x axis of image # {file[-5:-3]}")
+# plt.savefig("gaussian_fit_x.png", dpi=600)
 plt.show()
+
+# ------- Plotting the time evolution of the fitted parameters -------
+
+plt.rcParams.update({"font.size": 12})
+plt.subplot(2, 2, 1)
+plt.scatter(time_stamps, y0_values, label="y0")
+plt.xlabel("Time (s)")
+plt.ylabel(r"$\mu_y$")
+plt.subplot(2, 2, 2)
+plt.scatter(time_stamps, x0_values, label="x0")
+plt.xlabel("Time (s)")
+plt.ylabel(r"$\mu_x$")
+plt.subplot(2, 2, 3)
+plt.scatter(time_stamps, sigma_y_values, label="sigma_y")
+plt.xlabel("Time (s)")
+plt.ylabel(r"$\sigma_y$")
+plt.tight_layout()
+plt.subplot(2, 2, 4)
+plt.scatter(time_stamps, sigma_x_values, label="sigma_x")
+plt.xlabel("Time (s)")
+plt.ylabel(r"$\sigma_x$")
+plt.tight_layout()
+plt.subplots_adjust(top=0.92)
+# plt.savefig("gaussian_fit_parameters.png", dpi=600)
+plt.show()
+
+# ------- Compute the acceleration of the atomic cloud -------
+
+start = np.where(np.array(file_indices) == 10)[0][0]
+new_indices = file_indices[start:]
+t = dt * np.arange(len(new_indices)) + start_tof
+
+
+# Fit quadratic function to x0 values
+def quadratic(t, a, b, c):
+    return a * t**2 + b * t + c
+
+
+popt_x_accel, cov_x = curve_fit(quadratic, t, x0_values[start:])
+popt_y_accel, cov_y = curve_fit(quadratic, t, y0_values[start:])
+
+# Plotting the quadratic fits
+plt.figure(figsize=(10, 5))
+plt.rcParams.update({"font.size": 14})
+plt.subplot(1, 2, 1)
+plt.plot(t, x0_values[start:], "o", label=r"$\mu_x$ measured")
+plt.plot(t, quadratic(t, *popt_x_accel), label="Quadratic Fit", linestyle="--")
+plt.xlabel("Time (s)")
+plt.ylabel(r"$\mu_x$")
+plt.title(r"Quadratic Fit for $\mu_x(t)$")
+plt.legend()
+plt.subplot(1, 2, 2)
+plt.plot(t, y0_values[start:], "o", label=r"$\mu_y$ measured")
+plt.plot(t, quadratic(t, *popt_y_accel), label="Quadratic Fit", linestyle="--")
+plt.xlabel("Time (s)")
+plt.ylabel(r"$\mu_y$")
+plt.title(r"Quadratic Fit for $\mu_y(t)$")
+plt.legend()
+# plt.savefig("quadratic_fit_parameters.png", dpi=600)
+plt.tight_layout()
+
+# ------- Compute pixel size from acceleration -------
+
+# Estimating Pixel size
+g = 9.81  # m/s^2
+a_x = popt_x_accel[0]  # m/s^2
+a_y = popt_y_accel[0]  # m/s^2
+
+# Uncertainties
+a_x_err = np.sqrt(cov_x[0, 0])
+a_y_err = np.sqrt(cov_y[0, 0])
+
+pixel_size_est = g / (2 * np.sqrt(a_x**2 + a_y**2)) * 1e6  # µm
+
+partial_ax = -g * a_x / (2 * (a_x**2 + a_y**2) ** (3 / 2)) * 1e6  # µm
+partial_ay = -g * a_y / (2 * (a_x**2 + a_y**2) ** (3 / 2)) * 1e6  # µ
+
+pixel_size_error = np.sqrt((partial_ax**2 * a_x_err) + (partial_ay**2 * a_y_err))
+
+print("\nEstimated pixel size:", pixel_size_est.round(6), "µm")
+print("Pixel size error:", round(pixel_size_error, 6), "µm")
+
+# ------ Compute Temperature from the evolution of sigma value ------
+
+pixel_size = 3.45 * 1e-6  # in meters, actual pixel size
+# pixel_size = pixel_size_est * 1e-6  # convert µm to meters
+
+sigma_x_values_meters = np.array(sigma_x_values[start:]) * pixel_size
+sigma_y_values_meters = np.array(sigma_y_values[start:]) * pixel_size
+
+u = 1.66053906660e-27  # kg
+m_85rb = 84.91178974 * u  # kg
+k_B = 1.380649e-23  # J/K
+
+
+def sigma_evolution(t, sigma_0, T):
+    return np.sqrt(sigma_0**2 + (k_B * T / m_85rb) * t**2)
+
+
+# Fit the sigma evolution
+popt_sigma_x, cov_sigma_x = curve_fit(
+    sigma_evolution, t, sigma_x_values_meters, p0=[0.1, 0], bounds=(0, [np.inf, np.inf])
+)
+popt_sigma_y, cov_sigma_y = curve_fit(
+    sigma_evolution, t, sigma_y_values_meters, p0=[0.1, 0], bounds=(0, [np.inf, np.inf])
+)
+
+# Plotting the sigma evolution
+plt.figure(figsize=(10, 5))
+plt.subplot(1, 2, 1)
+plt.plot(t, sigma_x_values_meters, "o", label=r"$\sigma_x$ measured")
+plt.plot(t, sigma_evolution(t, *popt_sigma_x), label="Fit", linestyle="--")
+plt.xlabel("Time (s)")
+plt.ylabel(r"$\sigma_x$ (m)")
+plt.title(r"Sigma Evolution for $\sigma_x(t)$")
+plt.legend()
+plt.subplot(1, 2, 2)
+plt.plot(t, sigma_y_values_meters, "o", label=r"$\sigma_y$ measured")
+plt.plot(t, sigma_evolution(t, *popt_sigma_y), label="Fit", linestyle="--")
+plt.xlabel("Time (s)")
+plt.ylabel(r"$\sigma_y$ (m)")
+plt.title(r"Sigma Evolution for $\sigma_y(t)$")
+plt.legend()
+plt.tight_layout()
+# plt.savefig("sigma_evolution.png", dpi=600)
+
+T_opt = popt_sigma_x[1]
+T_err = np.sqrt(cov_sigma_x[1][1])
+print(f"\nEstimated temperature from x: {T_opt * 1e3:.6f} mK")
+print(f"Uncertainty: {T_err * 1e3:.6f} mK")
+
+T_opt = popt_sigma_y[1]
+T_err = np.sqrt(cov_sigma_y[1][1])
+print(f"\nEstimated temperature from y: {T_opt * 1e3:.6f} mK")
+print(f"Uncertainty: {T_err * 1e3:.6f} mK")
---
+++ ReleaseRecapture/mot_evaluation.py
@@ -0,0 +1,159 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import csv
+from scipy.special import erf
+from scipy.optimize import curve_fit
+import os, sys
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(script_dir)
+
+
+if __name__ == "__main__":
+    # Down times we used for the measurements
+    down_times = 1e-3 * np.array(
+        [5, 3, 7, 4, 8, 10, 12, 15, 17, 19, 20, 23, 27, 32, 36, 45, 65, 75, 85]
+    )
+    down_times_ms = down_times * 1e3
+
+    # Time stamps we chose by hand
+    # The time stamps define the time intervals for averaging the measurements
+    time_stamps = np.array(
+        [
+            [0.19, 0.22, 0.3, 0.52, 0.56],  # 5ms
+            [0.18, 0.2, 0.28, 0.5, 0.54],  # 3ms
+            [0.17, 0.22, 0.28, 0.5, 0.54],  # 7ms
+            [0.18, 0.21, 0.28, 0.5, 0.55],  # 4ms
+            [0.1, 0.14, 0.18, 0.43, 0.5],  # 8ms
+            [0.1, 0.16, 0.21, 0.43, 0.5],  # 10ms
+            [0.1, 0.15, 0.2, 0.43, 0.51],  # 12ms
+            [0.1, 0.17, 0.21, 0.43, 0.5],  # 15ms
+            [0.1, 0.17, 0.21, 0.43, 0.5],  # 17ms
+            [0.1, 0.17, 0.21, 0.43, 0.5],  # 19ms
+            [0.095, 0.17, 0.21, 0.43, 0.5],  # 20ms
+            [0.085, 0.16, 0.21, 0.43, 0.5],  # 23ms
+            [0.085, 0.16, 0.21, 0.43, 0.5],  # 27ms
+            [0.085, 0.15, 0.21, 0.43, 0.5],  # 32ms
+            [0.08, 0.15, 0.21, 0.43, 0.5],  # 36ms
+            [0.16, 0.25, 0.3, 0.52, 0.58],  # 45ms
+            [0.13, 0.25, 0.3, 0.52, 0.58],  # 65ms
+            [0.12, 0.25, 0.3, 0.52, 0.58],  # 75ms
+            [0.12, 0.25, 0.3, 0.52, 0.58],  # 85ms
+        ]
+    )
+
+    avg_ratios = np.zeros(len(time_stamps))
+    channel = 2
+
+    # i is the index of the time stamps
+    # j_file is the index of the file, since we made 4 measurements for each time stamp
+    for i in range(0, len(time_stamps)):
+        ratio_sum = 0
+        for j_file in range(4):
+            file_index = i * 4 + j_file + 1
+            file_path = os.path.join(script_dir, "Data", f"NewFile{file_index}.csv")
+            with open(file_path, newline="") as f:
+                reader = csv.reader(f)
+                next(reader)
+                second_row = next(reader)
+                increment = float(second_row[-1])
+                next(reader)
+
+                data = []
+                for row in reader:
+                    values = [float(x) for x in row if x.strip()]
+                    data.append(values)
+
+            data = np.array(data)
+
+            down_time = down_times[i]
+
+            ch = data[:, channel - 1]
+            t = np.arange(len(ch)) * increment
+
+            # Calculate the mean values for the left, middle, and offset regions
+            mean_left = np.mean(ch[t < time_stamps[i, 0]])
+            mean_middle = np.mean(ch[(t > time_stamps[i, 1]) & (t < time_stamps[i, 2])])
+            offset = np.mean(ch[(t > time_stamps[i, 3]) & (t < time_stamps[i, 4])])
+
+            N_0 = mean_left - offset
+            N_t = mean_middle - offset
+            ratio = N_t / N_0
+            ratio_sum += ratio
+
+            t_min = 0
+            t_max = 1
+
+            index_range = (t > t_min) & (t < t_max)
+
+            # Plotting the signal and averages for a specific example
+            i_example = 8
+            if i == i_example and j_file == 0:
+                plt.plot(t[index_range], ch[index_range])
+                # Plot averages
+                plt.plot(
+                    t[t < time_stamps[i, 0]],
+                    mean_left * np.ones_like(t[t < time_stamps[i, 0]]),
+                    "r",
+                )
+                plt.plot(
+                    t[(t > time_stamps[i, 1]) & (t < time_stamps[i, 2])],
+                    mean_middle
+                    * np.ones_like(
+                        t[(t > time_stamps[i, 1]) & (t < time_stamps[i, 2])]
+                    ),
+                    "r",
+                )
+                plt.plot(
+                    t[(t > time_stamps[i, 3]) & (t < time_stamps[i, 4])],
+                    offset
+                    * np.ones_like(
+                        t[(t > time_stamps[i, 3]) & (t < time_stamps[i, 4])]
+                    ),
+                    "r",
+                )
+
+                plt.xlabel("Time [s]")
+                # Round down times
+                plt.title(
+                    f"Example CH{channel} Signal with down time {round(down_times_ms[i])} ms"
+                )
+                plt.ylabel(f"Voltage [V]")
+                plt.grid(True)
+                plt.tight_layout()
+                plt.savefig(
+                    f"CH{channel}_down_time_{round(down_times_ms[i])}.png", dpi=300
+                )
+                plt.show()
+        avg_ratios[i] = ratio_sum / 4
+
+    ### Fit the curve
+    u = 1.66053906660e-27  # kg
+    m_85rb = 84.91178974 * u  # kg
+    k_B = 1.380649e-23  # J/K
+    R = 0.0125  # m
+
+    def f(t, A, T):
+        alpha = np.sqrt((2 * k_B * T) / m_85rb)
+        x = R / (alpha * t)
+        return A * (erf(x) - 2 / np.sqrt(np.pi) * x * np.exp(-(x**2)))
+
+    popt, pcov = curve_fit(f, down_times, avg_ratios, bounds=(0, [1, 1]), p0=[1, 0.1])
+
+    xaxis = np.linspace(0.001, down_times.max(), 1000)
+
+    plt.title(rf"Release and Recapture curve of trapped $^{{85}}Rb$ Atoms")
+    plt.scatter(down_times_ms, avg_ratios)
+    plt.plot(xaxis * 1000, f(xaxis, *popt), color="red", label="Fitted curve")
+    plt.xlabel("Down time [ms]")
+    plt.ylabel(rf"$I(t)$ / $I_0$")
+    plt.savefig("Release_and_Recapture.png", dpi=600)
+    plt.show()
+
+    print(r"Fitted equation: N(t)/N(0) = erf(χ(t)) - 2/√π χ(t) exp(-χ(t)^2)")
+    print(r"χ(t) = R / (α t), α = sqrt(2 k_B T / m)")
+
+    print(f"Fitted parameters: A = {popt[0]:.4f}, T = {1e3 * popt[1]:.4f} mK")
+    print(
+        f"Uncertainties: ΔA = {np.sqrt(pcov[0][0]):.4f}, ΔT = {1e3 * np.sqrt(pcov[1][1]):.4f} mK"
+    )
